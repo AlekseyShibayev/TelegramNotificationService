3fe99f664a9a5fed6d857a810f30dbc0cce34d27
Шибаев Алексей Евгеньевич (4094442) <VTB4094442@region.vtb.ru>
2023-08-30 23:03:48 +0300

    wb desire lot _ ref

--- src/main/java/com/company/app/wildberries_desire_lot/domain/service/FoundItemsService.java
+++ src/main/java/com/company/app/wildberries_desire_lot/domain/service/FoundItemsService.java
@@ -1,15 +1,30 @@
 package com.company.app.wildberries_desire_lot.domain.service;
 
 import com.company.app.wildberries_desire_lot.domain.entity.FoundItem;
+import com.company.app.wildberries_desire_lot.domain.repository.FoundItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 import java.util.List;
 
-public interface FoundItemsService {
+@Service
+@RequiredArgsConstructor
+public class FoundItemsService {
 
-    void save(FoundItem foundItem);
+    private final FoundItemRepository foundItemRepository;
 
-    void saveAll(Collection<FoundItem> foundItems);
+    public void save(FoundItem foundItem) {
+        foundItemRepository.save(foundItem);
+    }
 
-    List<FoundItem> getAll();
+    public void saveAll(Collection<FoundItem> foundItems) {
+        foundItemRepository.saveAll(foundItems);
+    }
+
+    public List<FoundItem> getAll() {
+        return foundItemRepository.findAll();
+    }
 }

ba19741b8bc190ecf2c1e4156f9d9a5a175cd459
Шибаев Алексей Евгеньевич (4094442) <VTB4094442@region.vtb.ru>
2023-08-30 23:24:04 +0300

    wb desire lot _ ref

---
+++ src/test/java/com/company/app/wildberries_desire_lot/component/WildberriesPriceExtractorTest.java
@@ -0,0 +1,27 @@
+package com.company.app.wildberries_desire_lot.component;
+
+import com.company.app.core.tool.api.DataExtractorTool;
+import com.company.app.springboottest.application.SpringBootTestApplicationContext;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class WildberriesPriceExtractorTest extends SpringBootTestApplicationContext {
+
+    private static final String FILE_NAME = "wildberries/response_example.json";
+
+    @Autowired
+    private WildberriesPriceExtractor wildberriesPriceExtractor;
+    @Autowired
+    private DataExtractorTool dataExtractorTool;
+
+    @Test
+    void extract() {
+        String fileAsString = dataExtractorTool.getFileAsString(FILE_NAME);
+
+        String price = wildberriesPriceExtractor.extract(fileAsString, "43409221");
+
+        Assertions.assertEquals("109900", price);
+    }
+
+}

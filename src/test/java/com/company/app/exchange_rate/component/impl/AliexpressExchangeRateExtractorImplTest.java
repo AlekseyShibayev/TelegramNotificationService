608f46012d9bf73471155c486529ad66a0b15b22
Шибаев Алексей Евгеньевич (4094442) <VTB4094442@region.vtb.ru>
2023-09-01 22:58:39 +0300

    - clean code

--- src/test/java/com/company/app/exchange_rate/component/impl/AliexpressExchangeRateExtractorImplTest.java
+++ src/test/java/com/company/app/exchange_rate/component/impl/AliexpressExchangeRateExtractorImplTest.java
@@ -6,26 +6,26 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 
 class AliexpressExchangeRateExtractorImplTest {
-
-    private static final String FILE_NAME = "exchangerate/response_example.json";
-
-    AliexpressExchangeRateExtractorImpl aliexpressExchangeRateExtractor;
-    DataExtractorTool dataExtractorTool;
-
-    @BeforeEach
-    public void init() {
-        dataExtractorTool = new DataExtractorToolImpl();
-        AliexpressReceiverImpl aliexpressReceiver = new AliexpressReceiverImpl();
-
-        aliexpressExchangeRateExtractor = new AliexpressExchangeRateExtractorImpl();
-        aliexpressExchangeRateExtractor.setAliexpressReceiver(aliexpressReceiver);
-        aliexpressExchangeRateExtractor.setDataExtractorTool(dataExtractorTool);
-    }
-
-    //    @Test
-    void extractTest() {
-        String aliexpressExchangeRate = aliexpressExchangeRateExtractor.getExchangeRate(dataExtractorTool.getFileAsString(FILE_NAME));
-        Assertions.assertNotNull(aliexpressExchangeRate);
-        Assertions.assertFalse(aliexpressExchangeRate.isEmpty());
-    }
+//
+//    private static final String FILE_NAME = "exchangerate/response_example.json";
+//
+//    AliexpressExchangeRateExtractorImpl aliexpressExchangeRateExtractor;
+//    DataExtractorTool dataExtractorTool;
+//
+//    @BeforeEach
+//    public void init() {
+//        dataExtractorTool = new DataExtractorToolImpl();
+//        AliexpressReceiverImpl aliexpressReceiver = new AliexpressReceiverImpl();
+//
+//        aliexpressExchangeRateExtractor = new AliexpressExchangeRateExtractorImpl();
+//        aliexpressExchangeRateExtractor.setAliexpressReceiver(aliexpressReceiver);
+//        aliexpressExchangeRateExtractor.setDataExtractorTool(dataExtractorTool);
+//    }
+//
+//    //    @Test
+//    void extractTest() {
+//        String aliexpressExchangeRate = aliexpressExchangeRateExtractor.getExchangeRate(dataExtractorTool.getFileAsString(FILE_NAME));
+//        Assertions.assertNotNull(aliexpressExchangeRate);
+//        Assertions.assertFalse(aliexpressExchangeRate.isEmpty());
+//    }
 }

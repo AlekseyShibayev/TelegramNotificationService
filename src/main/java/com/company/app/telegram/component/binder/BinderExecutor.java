afe0141a4cf4f7a19b306535c33911d94068c044
Шибаев Алексей Евгеньевич (4094442) <VTB4094442@region.vtb.ru>
2023-09-02 21:26:25 +0300

    - encapsulate in BinderRegistry done

--- src/main/java/com/company/app/telegram/component/binder/BinderExecutor.java
+++ src/main/java/com/company/app/telegram/component/binder/BinderExecutor.java
@@ -1,21 +1,11 @@
 package com.company.app.telegram.component.binder;
 
 import com.company.app.telegram.domain.entity.Chat;
-import com.company.app.telegram.domain.entity.Subscription;
-import com.company.app.telegram.domain.repository.SubscriptionRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import static com.company.app.telegram.component.binder.Binder.BINDER_DELIMITER;
 
@@ -26,32 +16,12 @@ import static com.company.app.telegram.component.binder.Binder.BINDER_DELIMITER;
 @RequiredArgsConstructor
 public class BinderExecutor {
 
-    private Map<String, Binder> binders; // todo encapsulate in BinderRegistry
-
-    private final SubscriptionRepository subscriptionRepository;
-    private final List<Binder> binderList;
-
-    @PostConstruct
-    void init() {
-        binders = binderList.stream()
-                .collect(Collectors.toMap(Binder::getType, Function.identity()));
-        if (binderList.size() != binders.keySet().size()) {
-            throw new DuplicateKeyException("ты конечно красиво написал, но дубли не проверил!");
-        }
-    }
-
-    @EventListener({ContextRefreshedEvent.class})
-    void saveSubscriptions() {
-        List<Subscription> subscriptionList = binders.keySet().stream()
-                .map(binderType -> Subscription.builder().type(binderType).build())
-                .collect(Collectors.toList());
-        subscriptionRepository.saveAll(subscriptionList);
-    }
+    private final BinderRegistry binderRegistry;
 
     public void execute(Chat chat, String text) {
         Optional<String> first = Arrays.stream(text.split(BINDER_DELIMITER)).findFirst();
 
-        Binder binder = Optional.ofNullable(binders.get(first.get()))
+        Binder binder = Optional.ofNullable(binderRegistry.get(first.get()))
                 .orElseThrow(() -> new IllegalArgumentException(String.format("Не смог вытащить тип binderType из [%s].", text)));
 
         BinderContext binderContainer = BinderContext.builder()
---
+++ src/main/java/com/company/app/telegram/component/binder/BinderRegistry.java
@@ -0,0 +1,53 @@
+package com.company.app.telegram.component.binder;
+
+import com.company.app.telegram.domain.entity.Subscription;
+import com.company.app.telegram.domain.repository.SubscriptionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class BinderRegistry {
+
+    private final Map<String, Binder> binderMap = new HashMap<>();
+
+    private final SubscriptionRepository subscriptionRepository;
+    private final List<Binder> binderList;
+
+    @PostConstruct
+    void init() {
+        for (Binder binder : binderList) {
+            if (binderMap.containsKey(binder.getType())) {
+                throw new DuplicateKeyException("developer! check binders for unique type, current type [%s]".formatted(binder.getType()));
+            } else {
+                binderMap.put(binder.getType(), binder);
+            }
+        }
+    }
+
+    @EventListener({ContextRefreshedEvent.class})
+    void saveSubscriptions() {
+        List<Subscription> subscriptionList = binderMap.keySet().stream()
+                .map(binderType -> Subscription.builder().type(binderType).build())
+                .toList();
+        subscriptionRepository.saveAll(subscriptionList);
+    }
+
+    public Binder get(String binderType) {
+        Binder binder = binderMap.get(binderType);
+        if (binder != null) {
+            return binder;
+        } else {
+            throw new UnsupportedOperationException("developer! impl this binder type [%s]".formatted(binderType));
+        }
+    }
+
+}

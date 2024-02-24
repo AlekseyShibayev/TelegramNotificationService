1d2233f5edc68e8583f39f8d2d3e6997f5bcbb67
Шибаев Алексей Евгеньевич (4094442) <VTB4094442@region.vtb.ru>
2024-02-25 02:23:58 +0300

    - add selenium property

--- src/main/java/com/company/app/common/selenium/service/SeleniumWebDriver.java
+++ src/main/java/com/company/app/common/selenium/service/SeleniumWebDriver.java
@@ -4,22 +4,12 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import net.lingala.zip4j.ZipFile;
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.time.Duration;
 
 @Slf4j
@@ -29,53 +19,29 @@ public class SeleniumWebDriver {
 
     public static final String DRIVER_PATH = "webdriver.chrome.driver";
 
+    @Value("${selenium.driver.path}")
+    private String driverPath;
+    @Value("${selenium.browser.path}")
+    private String browserPath;
+
     @Getter
     private ChromeDriver driver;
 
     @PostConstruct
     @SneakyThrows
     public void init() {
-//        File file = new File("selenium/chrome/chrome");
-//        if (Files.notExists(Paths.get("selenium/chrome/chrome")) {
-//
-//
-//
-//        }
-//
-//        FileUtils.deleteDirectory(new File("selenium"));
-//
-//        load("https://storage.googleapis.com/chrome-for-testing-public/122.0.6261.69/linux64/chromedriver-linux64.zip");
-//        load("https://storage.googleapis.com/chrome-for-testing-public/122.0.6261.69/linux64/chrome-linux64.zip");
-
         this.driver = createChromeDriver();
     }
 
-    private void load(String url) throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .GET()
-                .build();
-        InputStream inputStream = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofInputStream()).body();
-        File tempZip = new File("temp.zip");
-        FileUtils.copyInputStreamToFile(inputStream, tempZip);
-        try (ZipFile zipFile = new ZipFile(tempZip)) {
-            zipFile.extractAll("selenium/122.0.6261.69");
-        }
-        FileUtils.forceDelete(tempZip);
-    }
-
     private ChromeDriver createChromeDriver() {
-        File chromedriverFile = new File("selenium/chromedriver");
-        System.setProperty(DRIVER_PATH, chromedriverFile.getAbsolutePath());
+        System.setProperty(DRIVER_PATH, driverPath);
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 //        options.addArguments(" --window-size=1920,1080");
 //        options.addArguments("--headless");
 
-        if (Files.exists(Paths.get("selenium/chrome/chrome"))) {
-            options.setBinary("selenium/chrome/chrome");
-        }
+        options.setBinary(browserPath);
 
 //        options.addArguments("--silent");
 //        options.addArguments("--disable-logging");

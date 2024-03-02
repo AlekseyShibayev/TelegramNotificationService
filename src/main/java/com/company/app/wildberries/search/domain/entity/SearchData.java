bc2bd79b2d872f955e9b413182f824f5b3051708
Шибаев Алексей Евгеньевич (4094442) <VTB4094442@region.vtb.ru>
2024-03-03 00:37:41 +0300

    - isNeedLoadPriceHistory now see at created date

--- src/main/java/com/company/app/wildberries/search/domain/entity/SearchData.java
+++ src/main/java/com/company/app/wildberries/search/domain/entity/SearchData.java
@@ -3,12 +3,19 @@ package com.company.app.wildberries.search.domain.entity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.ZonedDateTime;
 
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 
 @Getter
@@ -16,14 +23,17 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 @Entity
 @Table(name = "SEARCH_DATA")
-public class SearchData {
+public class SearchData implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID", nullable = false)
     private Long id;
 
-    @Column(name = "CHAT_NAME")
+    @Column(name = "CHAT_NAME", nullable = false)
     private String chatName;
 
     @Column(name = "GENDER")
@@ -37,5 +47,13 @@ public class SearchData {
 
     @Column(name = "GREED_INDEX")
     private String greedIndex;
+
+    @CreationTimestamp
+    @Column(name = "CREATE_DATE", nullable = false)
+    private ZonedDateTime createDate;
+
+    @UpdateTimestamp
+    @Column(name = "UPDATE_DATE", nullable = false)
+    private ZonedDateTime updateDate;
 
 }

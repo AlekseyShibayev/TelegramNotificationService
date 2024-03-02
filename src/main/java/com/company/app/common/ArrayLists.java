b9c676e930deba47cd5ef4fecde227fad5e9a07c
Шибаев Алексей Евгеньевич (4094442) <VTB4094442@region.vtb.ru>
2024-03-02 23:18:15 +0300

    - isNeedLoadPriceHistory now see at created date

--- src/main/java/com/company/app/common/ArrayLists.java
+++ src/main/java/com/company/app/common/ArrayLists.java
@@ -7,6 +7,7 @@ import java.util.stream.Stream;
 
 public interface ArrayLists {
 
+    @SafeVarargs
     static <T> ArrayList<T> create(T... t) {
         return Stream.of(t).collect(Collectors.toCollection(ArrayList::new));
     }
